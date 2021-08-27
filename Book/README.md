5433b023bf404d796dbc1f5cb30ba99758d64819
FrediBrizuela <fbrizuela1@student.rccd.edu>
2021-08-26 19:30:49 -0700

    Signed-off-by: FrediBrizuela <fbrizuela1@student.rccd.edu>

---
+++ Book/README.md
@@ -0,0 +1,2 @@
+# BrizuelaFredi_CIS17A_48591
+Github repository for Fredi Brizuela for CIS-17A-48591
---
+++ Lab/BrizuelaFredi_HelloWorld/main.cpp
@@ -0,0 +1,36 @@
+/* 
+ * File:   main.cpp
+ * Author:Fredi Brizuela
+ * Created on August 26, 2021, 6:21 PM
+ * Purpose: Hello World
+ */
+
+//System Level Libraries 
+#include <iostream> //I/O Library
+using namespace std; //Libraries compiled under std
+
+//User Level Libraries
+
+//Global Constants - Science/Math Related
+//Conversions, Higher Dimensions
+
+//Function Prototype
+
+//Execution Begins Here !
+int main(int argc, char** argv) {
+    //Random Number Seed Set Here
+    
+    //Variable Declarations
+    
+    //Variable Initialization
+    
+    //Mapping Process Inputs to Outputs
+    
+    //Display Outputs
+    cout<<"Hello World"<<endl;
+    //Clean Up
+    
+    //Exit Stage Right
+    return 0;
+}
+
